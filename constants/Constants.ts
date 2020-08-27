49c38ae6fb85ea5b7d4239a930f9ce9b0c6d0c6e
shin donggyu <donggyu9410@gmail.com>
2020-08-28 03:34:02 +0900

    Delete NAVIGATION TYPE

--- constants/Constants.ts
+++ constants/Constants.ts
@@ -1,14 +1,1 @@
 export const ENDPOINT = `http://210.123.254.17:5001/api`;
-
-export const NAVIGATION_NAMES = {
-  mainStackNavigation: {
-    home: {
-      name: 'home',
-      showName: '맛집찾아줘',
-    },
-    list: {
-      name: 'list',
-      showName: '동적으로 처리',
-    },
-  },
-};
